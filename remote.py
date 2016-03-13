b4c408b0bdd27b7302c169c945e0adac67623e04
Ioan Loosley <legit.ioangogo@gmail.com>
2016-03-13 17:28:07 +0000

    Update remote.py

--- remote.py
+++ remote.py
@@ -1,1 +1,19 @@
+from flask import Flask
+import lirc
+app = Flask(__name__)
+deviceids = lirc.getids()
+@app.route('/')
+def index():
+    global deviceids
+    devices = []
+    for dev in deviceids:
+        d = {
+            'id': dev,
+            'name': dev,
+        }
+        devices.append(d)
+        return render_template('index.html', devices=devices)
+    
 
+if __name__ == '__main__':
+    app.run()
