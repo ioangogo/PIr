9d9bf4685275637dfcdb17cd977bedf1c000d139
Ioan Loosley <legit.ioangogo@gmail.com>
2016-03-13 17:33:06 +0000

    Update lirc.py

--- lirc.py
+++ lirc.py
@@ -1,1 +1,10 @@
 def getids():
+ """
+ returns a array of all configured devices
+ """
+ cmd = 'irsend LIST "" ""'
+ proc = subprocess.Popen(cmd, shell=True,stderr=subprocess.STDOUT, stdout=sub$
+ tmp = proc.stdout.read()
+ ntmp = tmp.replace("irsend: ", "")
+ device = ntmp.split("\r\n")
+ return device
